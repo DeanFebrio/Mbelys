7bbae3fc0faab3d622c92ab0007cfcd6d1c947a5
DeanFebrio <febriodean@gmail.com>
2025-08-14 23:17:07 +0700

    add widget

--- lib/view/pages/login/login_page.dart
+++ lib/view/pages/login/login_page.dart
@@ -17,88 +17,90 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AuthBackgroundPage(
         title: "Masuk",
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 70,),
-            AuthTextInput(
-              hintText: "Masukkan email",
-            ),
-            const SizedBox(height: 10,),
-            AuthTextInput(
-              hintText: "Masukkan password",
-            ),
-            const SizedBox(height: 8,),
-            SizedBox(
-              width: 340,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: GestureDetector(
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (e) => const ForgotPage()
-                      )
-                    );
-                  },
-                  child: Text(
-                      "Lupa password?",
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.color3
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 70,),
+              AuthTextInput(
+                hintText: "Masukkan email",
+              ),
+              const SizedBox(height: 10,),
+              AuthTextInput(
+                hintText: "Masukkan password",
+              ),
+              const SizedBox(height: 8,),
+              SizedBox(
+                width: 340,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (e) => const ForgotPage()
+                        )
+                      );
+                    },
+                    child: Text(
+                        "Lupa password?",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.color3
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30,),
-            AuthButton(
-              textButton: "Masuk",
-            ),
-            const SizedBox(height: 30,),
-            AuthDivider(text: "Masuk dengan"),
-            const SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GoogleButton(),
-                const SizedBox(width: 20,),
-                FacebookButton()
-              ],
-            ),
-            const SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Belum memiliki akun? ",
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.color3
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    context.go(RouterPath.register);
-                  },
-                  child: Text(
-                    "Daftar",
+              const SizedBox(height: 30,),
+              AuthButton(
+                textButton: "Masuk",
+              ),
+              const SizedBox(height: 30,),
+              AuthDivider(text: "Masuk dengan"),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GoogleButton(),
+                  const SizedBox(width: 20,),
+                  FacebookButton()
+                ],
+              ),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Belum memiliki akun? ",
                     style: TextStyle(
                         fontSize: 12,
                         fontFamily: "Montserrat",
-                        fontWeight: FontWeight.w700,
+                        fontWeight: FontWeight.w600,
                         color: AppColors.color3
                     ),
                   ),
-                )
-              ],
-            )
-          ],
+                  GestureDetector(
+                    onTap: (){
+                      context.go(RouterPath.register);
+                    },
+                    child: Text(
+                      "Daftar",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.color3
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         )
     );
   }
--- lib/view/pages/register/register_page.dart
+++ lib/view/pages/register/register_page.dart
@@ -16,72 +16,74 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AuthBackgroundPage(
         title: "Daftar",
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 70,),
-            AuthTextInput(
-              hintText: "Masukkan email",
-            ),
-            const SizedBox(height: 10,),
-            AuthTextInput(
-              hintText: "Masukkan password",
-            ),
-            const SizedBox(height: 10,),
-            AuthTextInput(
-              hintText: "Masukkan nama",
-            ),
-            const SizedBox(height: 10,),
-            AuthTextInput(
-              hintText: "Masukkan nomor telepon",
-            ),
-            const SizedBox(height: 30,),
-            AuthButton(
-              textButton: "Daftar",
-            ),
-            const SizedBox(height: 30,),
-            AuthDivider(
-              text: "Daftar dengan",
-            ),
-            const SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GoogleButton(),
-                const SizedBox(width: 20,),
-                FacebookButton()
-              ],
-            ),
-            const SizedBox(height: 30,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Sudah memiliki akun? ",
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.color3
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    context.go(RouterPath.login);
-                  },
-                  child: Text(
-                    "Masuk",
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 70,),
+              AuthTextInput(
+                hintText: "Masukkan email",
+              ),
+              const SizedBox(height: 10,),
+              AuthTextInput(
+                hintText: "Masukkan password",
+              ),
+              const SizedBox(height: 10,),
+              AuthTextInput(
+                hintText: "Masukkan nama",
+              ),
+              const SizedBox(height: 10,),
+              AuthTextInput(
+                hintText: "Masukkan nomor telepon",
+              ),
+              const SizedBox(height: 30,),
+              AuthButton(
+                textButton: "Daftar",
+              ),
+              const SizedBox(height: 30,),
+              AuthDivider(
+                text: "Daftar dengan",
+              ),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GoogleButton(),
+                  const SizedBox(width: 20,),
+                  FacebookButton()
+                ],
+              ),
+              const SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Sudah memiliki akun? ",
                     style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.w700,
-                        color: AppColors.color3
+                      fontSize: 12,
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.color3
                     ),
                   ),
-                )
-              ],
-            )
-          ],
+                  GestureDetector(
+                    onTap: (){
+                      context.go(RouterPath.login);
+                    },
+                    child: Text(
+                      "Masuk",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.color3
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         )
     );
   }
