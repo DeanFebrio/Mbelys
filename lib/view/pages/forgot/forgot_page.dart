ff23693a61e2cace0377232dbcb797a3c6eb2e38
DeanFebrio <febriodean@gmail.com>
2025-08-15 00:03:51 +0700

    fix styling

--- lib/view/pages/forgot/forgot_page.dart
+++ lib/view/pages/forgot/forgot_page.dart
@@ -1,48 +1,53 @@
 import "package:flutter/material.dart";
 import "package:mbelys/core/constant/app_colors.dart";
-import "package:mbelys/view/pages/forgot/widgets/forgot_button.dart";
-import "package:mbelys/view/pages/forgot/widgets/forgot_text_input.dart";
+import "package:mbelys/view/pages/forgot/widgets/forgot_success_dialog.dart";
+import "package:mbelys/view/widgets/custom_background_page.dart";
+import "package:mbelys/view/widgets/custom_button.dart";
+import "package:mbelys/view/widgets/custom_text_input.dart";
 
 class ForgotPage extends StatelessWidget {
   const ForgotPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.color2,
-      appBar: AppBar(
-        backgroundColor: AppColors.color2,
-        title: Text(
-            "Lupa Password",
-          style: TextStyle(
-            fontSize: 32,
-            fontFamily: "Poppins",
-            fontWeight: FontWeight.w700,
-            color: AppColors.color9
+    final formTextStyle = TextStyle(
+        fontSize: 20,
+        fontFamily: "Montserrat",
+        fontWeight: FontWeight.w700,
+        color: AppColors.color9
+    );
+
+    return CustomBackgroundPage(
+      title: "Lupa Password",
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 120,),
+              Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Masukkan email",
+                      style: formTextStyle,
+                    ),
+                    const SizedBox(height: 5,),
+                    CustomTextInput(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30,),
+              CustomButton(
+                  onTap: () {
+                    showForgotSuccessDialog(context);
+                  },
+                  textButton: "Lanjut",
+                  textColor: AppColors.color3,
+                  backgroundColor: AppColors.color9
+              )
+            ],
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-                "Masukkan Email",
-              style: TextStyle(
-                fontSize: 20,
-                fontFamily: "Montserrat",
-                fontWeight: FontWeight.w700,
-                color: AppColors.color9
-              ),
-            ),
-            const SizedBox(height: 10,),
-            ForgotTextInput(hintText: "Ketikkan email"),
-            const SizedBox(height: 20,),
-            ForgotButton()
-          ],
-        ),
-      ),
     );
   }
 }
