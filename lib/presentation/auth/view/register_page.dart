6670ae84725a46b3d81a8138a2af1450dccaf18c
DeanFebrio <febriodean@gmail.com>
2025-08-21 23:40:58 +0700

    add Register Feature

--- lib/presentation/auth/view/register_page.dart
+++ lib/presentation/auth/view/register_page.dart
@@ -2,89 +2,210 @@ import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 import "package:mbelys/core/constant/app_colors.dart";
 import "package:mbelys/core/router/router.dart";
+import "package:mbelys/core/services/service_locator.dart";
+import "package:mbelys/presentation/auth/viewmodel/register_viewmodel.dart";
 import "package:mbelys/presentation/auth/widgets/auth_background_page.dart";
 import "package:mbelys/presentation/auth/widgets/auth_button.dart";
 import "package:mbelys/presentation/auth/widgets/auth_divider.dart";
 import "package:mbelys/presentation/auth/widgets/auth_text_input.dart";
 import "package:mbelys/presentation/auth/widgets/facebook_button.dart";
 import "package:mbelys/presentation/auth/widgets/google_button.dart";
+import "package:provider/provider.dart";
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AuthBackgroundPage(
-        title: "Daftar",
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return ChangeNotifierProvider(
+        create: (_) => sl<RegisterViewModel>(),
+      child: Consumer<RegisterViewModel>(
+        builder: (context, vm, child) {
+          return Stack(
             children: [
-              const SizedBox(height: 70,),
-              AuthTextInput(
-                hintText: "Masukkan email",
-              ),
-              const SizedBox(height: 10,),
-              AuthTextInput(
-                hintText: "Masukkan password",
-              ),
-              const SizedBox(height: 10,),
-              AuthTextInput(
-                hintText: "Masukkan nama",
-              ),
-              const SizedBox(height: 10,),
-              AuthTextInput(
-                hintText: "Masukkan nomor telepon",
-              ),
-              const SizedBox(height: 30,),
-              AuthButton(
-                textButton: "Daftar",
-              ),
-              const SizedBox(height: 30,),
-              AuthDivider(
-                text: "Daftar dengan",
-              ),
-              const SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GoogleButton(),
-                  const SizedBox(width: 20,),
-                  FacebookButton()
-                ],
-              ),
-              const SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Sudah memiliki akun? ",
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.color3
+              RegisterView(),
+              if (vm.state == RegisterState.loading)
+                Container(
+                  color: Colors.black12,
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
                     ),
                   ),
-                  GestureDetector(
-                    onTap: (){
-                      context.go(RouterPath.login);
-                    },
-                    child: Text(
-                      "Masuk",
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontFamily: "Montserrat",
-                          fontWeight: FontWeight.w700,
-                          color: AppColors.color3
-                      ),
-                    ),
-                  )
-                ],
-              )
+                ),
             ],
-          ),
-        )
+          );
+        }
+      )
     );
   }
 }
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<RegisterViewModel>();
+
+    return AuthBackgroundPage(
+      title: "Daftar",
+      child: SingleChildScrollView (
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 70,),
+            FormSection(vm: vm),
+            const SizedBox(height: 30,),
+            RegisterButton(vm: vm),
+            const SizedBox(height: 30,),
+            AuthDivider(
+              text: "Daftar dengan",
+            ),
+            const SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GoogleButton(),
+                const SizedBox(width: 20,),
+                FacebookButton()
+              ],
+            ),
+            const SizedBox(height: 30,),
+            LoginRedirect()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginRedirect extends StatelessWidget {
+  const LoginRedirect({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Sudah memiliki akun? ",
+          style: TextStyle(
+              fontSize: 12,
+              fontFamily: "Montserrat",
+              fontWeight: FontWeight.w600,
+              color: AppColors.color3
+          ),
+        ),
+        GestureDetector(
+          onTap: (){
+            context.go(RouterPath.login);
+          },
+          child: Text(
+            "Masuk",
+            style: TextStyle(
+                fontSize: 12,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.w700,
+                color: AppColors.color3
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({
+    super.key,
+    required this.vm,
+  });
+  final RegisterViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<RegisterViewModel>().state;
+
+    return AuthButton(
+      textButton: "Daftar",
+      onPressed: state == RegisterState.loading ? null : () async {
+        await vm.register();
+        if (!context.mounted) return;
+        if (vm.state == RegisterState.error) {
+          print("User nih: ${vm.user}");
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    vm.errorMessage ?? "Gagal mendaftar!",
+                    style: TextStyle(
+                      color: AppColors.color2,
+                      fontSize: 16,
+                      fontFamily: "Mulish",
+                      fontWeight: FontWeight.w700
+                    ),
+                  ),
+                ),
+                backgroundColor: AppColors.color8,
+              )
+          );
+        } else if (vm.state == RegisterState.success) {
+          print("User nih: ${vm.user?.name}");
+          context.go(RouterPath.login);
+        }
+      },
+    );
+  }
+}
+
+class FormSection extends StatelessWidget {
+  const FormSection({
+    super.key,
+    required this.vm,
+  });
+
+  final RegisterViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form (
+      key: vm.formKey,
+      child: Column (
+        children: [
+          AuthTextInput(
+            hintText: "Masukkan email",
+            controller: vm.emailController,
+            validator: vm.validateEmail,
+            isPassword: false,
+          ),
+          const SizedBox(height: 10,),
+          AuthTextInput(
+            hintText: "Masukkan password",
+            controller: vm.passwordController,
+            validator: vm.validatePassword,
+            isPassword: true,
+          ),
+          const SizedBox(height: 10,),
+          AuthTextInput(
+            hintText: "Masukkan nama",
+            controller: vm.nameController,
+            validator: vm.validateName,
+            isPassword: false,
+          ),
+          const SizedBox(height: 10,),
+          AuthTextInput(
+            hintText: "Masukkan nomor telepon",
+            controller: vm.phoneController,
+            validator: vm.validatePhone,
+            isPassword: false,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/presentation/auth/viewmodel/register_viewmodel.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mbelys/domain/entities/user_entity.dart';
+import 'package:mbelys/domain/usecases/register_usecase.dart';
+
+enum RegisterState { initial, loading, success, error }
+
+class RegisterViewModel extends ChangeNotifier {
+  final RegisterUseCase registerUseCase;
+  RegisterViewModel({required this.registerUseCase});
+
+  UserEntity? _user;
+  UserEntity? get user => _user;
+
+  RegisterState _state = RegisterState.initial;
+  RegisterState get state => _state;
+
+  String? _errorMessage;
+  String? get errorMessage => _errorMessage;
+
+  final formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
+
+  Future<void> register () async {
+    if (!formKey.currentState!.validate()) return;
+
+    _state = RegisterState.loading;
+    _errorMessage = null;
+    notifyListeners();
+
+    final result = await registerUseCase.call(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+        nameController.text.trim(),
+        phoneController.text.trim()
+    );
+    result.fold(
+        (failure) {
+          _errorMessage = failure.message;
+          _state = RegisterState.error;
+        },
+        (userEntity) {
+          _user = userEntity;
+          _state = RegisterState.success;
+        }
+    );
+    notifyListeners();
+  }
+
+  String? validateEmail (String? value){
+    if (value == null || value.isEmpty) return "Email Wajib diisi";
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!emailRegExp.hasMatch(value)) return "Format email tidak valid!";
+    return null;
+  }
+
+  String? validatePassword (String? value){
+    if (value == null || value.isEmpty) return "Password Wajib diisi";
+    if (value.length < 8) return "Password minimal 8 karakter";
+    if (!value.contains(RegExp(r'[A-Z]'))) return "Password harus memiliki 1 huruf kapital";
+    if (!value.contains(RegExp(r'[0-9]'))) return "Password harus memiliki 1 angka";
+    return null;
+  }
+
+  String? validateName (String? value){
+    if (value == null || value.isEmpty) return "Nama wajib diisi";
+    return null;
+  }
+
+  String? validatePhone (String? value) {
+    if (value == null || value.isEmpty) return "Nomor telepon wajib diisi";
+    if (!RegExp(r'^[0-9]+$').hasMatch(value)) return "Nomor telepon harus angka";
+    return null;
+  }
+}
