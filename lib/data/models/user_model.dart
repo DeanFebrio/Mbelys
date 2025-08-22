43863a0fc7e2f89768d4a68cb39924270fc4ae42
DeanFebrio <febriodean@gmail.com>
2025-08-22 18:52:53 +0700

    fix

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,22 +1,22 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mbelys/domain/entities/user_entity.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:mbelys/domain/entities/user_entity.dart';
 
-class UserModel extends UserEntity {
-  UserModel ({
-  required super.uid,
-  required super.email,
-  required super.name,
-  required super.phone,
-  });
+  class UserModel extends UserEntity {
+    UserModel ({
+    required super.uid,
+    required super.email,
+    required super.name,
+    required super.phone,
+    });
 
-  factory UserModel.fromFirebase(DocumentSnapshot snap){
-    final data = snap.data() as Map<String, dynamic>;
+    factory UserModel.fromFirebase(DocumentSnapshot snap){
+      final data = snap.data() as Map<String, dynamic>;
 
-    return UserModel(
-        uid: snap.id,
-        email: data['email'] ?? "",
-        name: data['name'] ?? "",
-        phone: data['phone'] ?? "",
-    );
+      return UserModel(
+          uid: snap.id,
+          email: data['email'] ?? "",
+          name: data['name'] ?? "",
+          phone: data['phone'] ?? "",
+      );
+    }
   }
-}
