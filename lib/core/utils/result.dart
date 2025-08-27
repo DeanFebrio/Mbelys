eb1841a72e7360bcc56539451abbd3fcf6d64699
DeanFebrio <febriodean@gmail.com>
2025-08-28 00:03:22 +0700

    add Either and Result Type Alias

--- lib/core/utils/result.dart
+++ lib/core/utils/result.dart
@@ -1,8 +1,17 @@
-abstract class Failure {
-  final String message;
-  const Failure(this.message);
-}
+import 'package:dartz/dartz.dart';
+import 'package:mbelys/core/error/failure.dart';
 
-class ServerFailure extends Failure {
-  const ServerFailure(String message) : super(message);
+typedef Result<T> = Either<Failure, T>;
+typedef AsyncResult<T> = Future<Result<T>>;
+
+Result<T> ok<T>(T value) => Right(value);
+Result<Unit> okUnit() => const Right(unit);
+Result<T> err<T>(Failure failure) => Left(failure);
+
+typedef VoidResult = Result<Unit>;
+typedef AsyncVoidResult = Future<VoidResult>;
+
+extension ResultX<T> on Result<T> {
+  T? getOrNull() => fold((_) => null, (r) => r);
+  Failure? failureOrNull() => fold((l) => l, (_) => null);
 }
