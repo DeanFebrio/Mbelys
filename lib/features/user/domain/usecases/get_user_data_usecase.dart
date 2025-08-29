95fa6e7af3bfa1807d632728ac7817b25182d0e2
DeanFebrio <febriodean@gmail.com>
2025-08-29 21:43:02 +0700

    Refactor GetUserDataUseCase

--- lib/features/user/domain/usecases/get_user_data_usecase.dart
+++ lib/features/user/domain/usecases/get_user_data_usecase.dart
@@ -1,17 +1,14 @@
 import 'package:mbelys/core/utils/result.dart';
-import 'package:mbelys/features/auth/domain/repositories/auth_repository.dart';
 import 'package:mbelys/features/user/domain/entities/user_entity.dart';
 import 'package:mbelys/features/user/domain/repositories/user_repository.dart';
 
 class GetUserDataUseCase {
   final UserRepository userRepository;
-  final AuthRepository authRepository;
   GetUserDataUseCase({
     required this.userRepository,
-    required this.authRepository
   });
 
   AsyncResult<UserEntity> call (String uid) async {
-    return await userRepository.getUserProfile(uid: uid);
+    return await userRepository.getUserData(uid: uid);
   }
 }
