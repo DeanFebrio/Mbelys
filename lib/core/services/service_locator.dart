7b08d6dd3f1b8aeab12c2a049b37aa0dad1ac86f
DeanFebrio <febriodean@gmail.com>
2025-08-29 21:42:37 +0700

    Refactor service locators

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,9 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mbelys/core/services/auth_locator.dart';
+import 'package:mbelys/core/services/data/init_auth_data.dart';
+import 'package:mbelys/core/services/data/init_user_data.dart';
+import 'package:mbelys/core/services/domain/init_auth_domain.dart';
+import 'package:mbelys/core/services/domain/init_user_domain.dart';
 import 'package:mbelys/core/services/home_locator.dart';
-import 'package:mbelys/core/services/user_locator.dart';
+import 'package:mbelys/core/services/presentation/init_auth_presentation.dart';
+import 'package:mbelys/core/services/presentation/init_user_presentation.dart';
 
 final sl = GetIt.instance;
 
@@ -11,7 +15,17 @@ Future<void> setupLocator () async {
   sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
 
-  await initAuth();
-  await initUser();
+  // data layer
+  await initAuthData();
+  await initUserData();
+
+  // domain layer
+  await initAuthDomain();
+  await initUserDomain();
+
+  // presentation layer
+  initAuthPresentation();
+  initUserPresentation();
+
   await initHome();
 }
