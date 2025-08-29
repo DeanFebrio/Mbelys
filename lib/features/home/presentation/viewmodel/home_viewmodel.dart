8dd525c630f43e100e34638945cd8c55dac1d85e
DeanFebrio <febriodean@gmail.com>
2025-08-29 21:40:33 +0700

    Refactor HomeViewModel to use ProfileViewModel

--- lib/features/home/presentation/viewmodel/home_viewmodel.dart
+++ lib/features/home/presentation/viewmodel/home_viewmodel.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
-import 'package:mbelys/features/auth/presentation/viewmodels/auth_viewmodel.dart';
 import 'package:mbelys/features/user/domain/entities/user_entity.dart';
+import 'package:mbelys/features/user/presentation/viewmodel/profile_viewmodel.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  final AuthViewModel _authViewModel;
+  final ProfileViewModel _profileViewModel;
 
   HomeViewModel ({
-    required AuthViewModel authViewModel
-  }) : _authViewModel = authViewModel;
+    required ProfileViewModel profileViewModel
+  }) : _profileViewModel = profileViewModel;
 
-  UserEntity? get user => _authViewModel.user;
+  UserEntity? get user => _profileViewModel.user;
   String? get name => user?.name;
 }
