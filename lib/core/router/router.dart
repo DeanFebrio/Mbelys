8423b0f3227722afa43197165dbe367330e72466
DeanFebrio <febriodean@gmail.com>
2025-08-31 20:31:27 +0700

    Update RouterPath and RouterName
    - Add comments to group routes by feature
    - Add `email` route to user feature

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,31 +1,51 @@
 class RouterPath {
+  // auth feature
   static const welcome = '/welcome';
   static const login = '/login';
   static const forgot = '/forgot';
   static const register = '/register';
+
+  // home feature
   static const home = '/home';
+
+  // kandang feature
   static const detail = '/detail';
   static const add = '/add';
+
+  // user feature
   static const profile = '/profile';
   static const editProfile = '/editProfil';
   static const password = '/password';
+  static const email = '/email';
   static const feedback = '/feedback';
+
+  // common
   static const error = '/error';
   static const splash = '/splash';
 }
 
 class RouterName {
+  // auth feature
   static const welcome = 'welcome';
   static const login = 'login';
   static const forgot = 'forgot';
   static const register = 'register';
+
+  // home feature
   static const home = 'home';
+
+  // kandang feature
   static const detail = 'detail';
   static const add = 'add';
+
+  // user feature
   static const profile = 'profile';
   static const editProfile = 'editProfil';
   static const password = 'password';
+  static const email = 'email';
   static const feedback = 'feedback';
+
+  // common
   static const error = 'error';
   static const splash = 'splash';
 }
