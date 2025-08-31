4c890edc3b9b9084eaaa93e4c025e5fd0b54cdf0
DeanFebrio <febriodean@gmail.com>
2025-08-31 20:12:41 +0700

    Add isLoading state to CustomShortButton

--- lib/presentation/widgets/custom_short_button.dart
+++ lib/presentation/widgets/custom_short_button.dart
@@ -4,25 +4,38 @@ import "package:mbelys/core/constant/app_colors.dart";
 class CustomShortButton extends StatelessWidget {
   final VoidCallback? onTap;
   final String buttonText;
+  final bool isLoading;
 
   const CustomShortButton({
     super.key,
     this.onTap,
-    required this.buttonText
+    required this.buttonText,
+    this.isLoading = false
   });
 
   @override
   Widget build(BuildContext context) {
     return FilledButton(
-        onPressed: onTap,
+        onPressed: isLoading ? null : onTap,
         style: FilledButton.styleFrom(
             backgroundColor: AppColors.color9,
+            disabledBackgroundColor: AppColors.color9,
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10)
             )
         ),
-        child: Text(
+        child: isLoading ?
+        SizedBox(
+          height: 10,
+          width: 10,
+          child: CircularProgressIndicator(
+            color: AppColors.color2,
+            backgroundColor: Colors.transparent,
+          ),
+        )
+        :
+        Text(
           buttonText,
           style: TextStyle(
               fontSize: 16,
