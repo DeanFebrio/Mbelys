f30ba1abfee1bbccb99ac7c28d3493dbfd660979
DeanFebrio <febriodean@gmail.com>
2025-09-02 22:05:18 +0700

    add change password feature

--- lib/features/user/presentation/pages/password_page.dart
+++ lib/features/user/presentation/pages/password_page.dart
@@ -2,21 +2,41 @@ import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 import "package:mbelys/core/constant/app_colors.dart";
 import "package:mbelys/core/router/router.dart";
+import "package:mbelys/core/services/service_locator.dart";
+import "package:mbelys/features/user/presentation/viewmodel/password_viewmodel.dart";
 import "package:mbelys/presentation/widgets/custom_background_page.dart";
+import "package:mbelys/presentation/widgets/custom_dialog.dart";
 import "package:mbelys/presentation/widgets/custom_long_button.dart";
+import "package:mbelys/presentation/widgets/custom_snackbar.dart";
 import "package:mbelys/presentation/widgets/custom_text_input.dart";
+import "package:provider/provider.dart";
 
 class PasswordPage extends StatelessWidget {
   const PasswordPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final formTextStyle = TextStyle(
-      fontSize: 20,
-      fontFamily: "Montserrat",
-      fontWeight: FontWeight.w700,
-      color: AppColors.color9
+    return ChangeNotifierProvider(
+      create: (_) => sl<PasswordViewModel>(),
+      child: const PasswordView(),
     );
+  }
+}
+
+class PasswordView extends StatelessWidget {
+  const PasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final formTextStyle = TextStyle(
+        fontSize: 20,
+        fontFamily: "Montserrat",
+        fontWeight: FontWeight.w700,
+        color: AppColors.color9
+    );
+
+    final vm = context.watch<PasswordViewModel>();
+    final state = vm.state;
 
     return CustomBackgroundPage(
       title: "Ubah Password",
@@ -25,44 +45,84 @@ class PasswordPage extends StatelessWidget {
           children: [
             const SizedBox(height: 120,),
             Form(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Password Lama",
-                      style: formTextStyle,
-                    ),
-                    const SizedBox(height: 5,),
-                    CustomTextInput(),
-                    const SizedBox(height: 10,),
-                    Text(
-                      "Password Baru",
-                      style: formTextStyle,
-                    ),
-                    const SizedBox(height: 5,),
-                    CustomTextInput(),
-                    const SizedBox(height: 10,),
-                    Text(
-                      "Konfirmasi Password",
-                      style: formTextStyle,
-                    ),
-                    const SizedBox(height: 5,),
-                    CustomTextInput(),
-                  ],
-                ),
+              key: vm.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Password Lama",
+                    style: formTextStyle,
+                  ),
+                  const SizedBox(height: 5,),
+                  CustomTextInput(
+                    isPassword: true,
+                    textEditingController: vm.oldPasswordController,
+                  ),
+                  const SizedBox(height: 10,),
+                  Text(
+                    "Password Baru",
+                    style: formTextStyle,
+                  ),
+                  const SizedBox(height: 5,),
+                  CustomTextInput(
+                    isPassword: true,
+                    textEditingController: vm.newPasswordController,
+                    validator: vm.validatePassword,
+                  ),
+                  const SizedBox(height: 10,),
+                  Text(
+                    "Konfirmasi Password",
+                    style: formTextStyle,
+                  ),
+                  const SizedBox(height: 5,),
+                  CustomTextInput(
+                    isPassword: true,
+                    textEditingController: vm.confirmPasswordController,
+                    validator: vm.validateConfirmPassword,
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 30,),
             CustomLongButton(
-                onTap: () {
-                  context.go(RouterPath.profile);
+                onTap: state == PasswordState.loading ? null : () async {
+                  await vm.changePassword();
+                  if (!context.mounted) return;
+                  if (vm.state == PasswordState.error) {
+                    showErrorSnackBar(context, vm.errorMessage);
+                  } else if (vm.state == PasswordState.success){
+                    customDialog(
+                        context,
+                        "Password berhasil diubah",
+                        "Anda dapat menggunakan password baru untuk masuk aplikasi",
+                        "Baik",
+                        RouterPath.home
+                    );
+                  }
                 },
                 textButton: "Simpan",
                 textColor: AppColors.color3,
                 backgroundColor: AppColors.color9
-            )
+            ),
+            const SizedBox(height: 30,),
+            GestureDetector(
+              onTap: (){
+                context.push(RouterPath.forgot);
+              },
+              child: Text(
+                "Lupa password?",
+                style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.color8
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
---
+++ lib/features/user/presentation/viewmodel/password_viewmodel.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mbelys/core/error/failure.dart';
+import 'package:mbelys/core/utils/result.dart';
+import 'package:mbelys/features/auth/domain/usecases/change_password_usecase.dart';
+
+enum PasswordState { initial, loading, success, error }
+
+class PasswordViewModel extends ChangeNotifier {
+  final ChangePasswordUseCase changePasswordUseCase;
+
+  PasswordViewModel({
+    required this.changePasswordUseCase
+  });
+
+  PasswordState _state = PasswordState.initial;
+  PasswordState get state => _state;
+
+  String? _errorMessage;
+  String? get errorMessage => _errorMessage;
+
+  final formKey = GlobalKey<FormState>();
+
+  final oldPasswordController = TextEditingController();
+  final newPasswordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  String? validatePassword (String? value) {
+    if (value == null || value.isEmpty) return "Password Baru Wajib diisi";
+    if (value.length < 8) return "Password minimal 8 karakter";
+    if (!value.contains(RegExp(r'[A-Z]'))) return "Password harus memiliki 1 huruf kapital";
+    if (!value.contains(RegExp(r'[0-9]'))) return "Password harus memiliki 1 angka";
+
+    final oldPassword = oldPasswordController.text.trim();
+    if (oldPassword == value) return "Password baru sama dengan yang lama";
+    return null;
+  }
+
+  String? validateConfirmPassword (String? value) {
+    final newPass = newPasswordController.text.trim();
+    if (value == null || value.isEmpty) return "Konfirmasi Password Wajib diisi";
+    if (value != newPass) return "Password tidak sama";
+    return null;
+  }
+
+  AsyncVoidResult changePassword () async {
+    if (!formKey.currentState!.validate()) return err(AuthFailure("Pendaftaran gagal!"));
+
+    _state = PasswordState.loading;
+    _errorMessage = null;
+    notifyListeners();
+
+    final oldPassword = oldPasswordController.text.trim();
+    final newPassword = newPasswordController.text.trim();
+    final result = await changePasswordUseCase.call(oldPassword: oldPassword, newPassword: newPassword);
+
+    result.fold(
+        (failure) {
+          _errorMessage = failure.message;
+          _state = PasswordState.error;
+        },
+        (userEntity) {
+          _state = PasswordState.success;
+        }
+    );
+    notifyListeners();
+    return okUnit();
+  }
+
+}
