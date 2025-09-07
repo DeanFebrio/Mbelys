c2e118a75304d61547fcf5e111eb2af661464cb1
DeanFebrio <febriodean@gmail.com>
2025-09-07 23:40:17 +0700

    Remove HomeLocator

--- lib/core/services/home_locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mbelys/core/services/service_locator.dart';
-import 'package:mbelys/features/home/presentation/viewmodel/home_viewmodel.dart';
-import 'package:mbelys/features/user/presentation/viewmodels/profile_viewmodel.dart';
-
-Future<void> initHome () async {
-  sl.registerFactory<HomeViewModel>(() => HomeViewModel(
-      profileViewModel: sl<ProfileViewModel>()
-  ));
-}
