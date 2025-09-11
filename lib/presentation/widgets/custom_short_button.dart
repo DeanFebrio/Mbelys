5a7eac798a0e13f79b081ff04bb0c4daf0fcd39b
DeanFebrio <febriodean@gmail.com>
2025-09-11 23:57:34 +0700

    Add new CustomShortButton parameter

--- lib/presentation/widgets/custom_short_button.dart
+++ lib/presentation/widgets/custom_short_button.dart
@@ -5,12 +5,14 @@ class CustomShortButton extends StatelessWidget {
   final VoidCallback? onTap;
   final String buttonText;
   final bool isLoading;
+  final bool isOnTop;
 
   const CustomShortButton({
     super.key,
     this.onTap,
     required this.buttonText,
-    this.isLoading = false
+    this.isLoading = false,
+    this.isOnTop = false
   });
 
   @override
@@ -18,8 +20,8 @@ class CustomShortButton extends StatelessWidget {
     return FilledButton(
         onPressed: isLoading ? null : onTap,
         style: FilledButton.styleFrom(
-            backgroundColor: AppColors.color9,
-            disabledBackgroundColor: AppColors.color9,
+            backgroundColor: isOnTop ? AppColors.color9.withValues(alpha: 0.5) : AppColors.color9,
+            disabledBackgroundColor: isOnTop ? AppColors.color9.withValues(alpha: 0.5) : AppColors.color9,
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10)
