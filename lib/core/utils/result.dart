293cc66cecd57238e8495ec59e2ffe6aee2eb84a
DeanFebrio <febriodean@gmail.com>
2025-09-15 21:57:16 +0700

    Add AsyncResult helpers and extensions

--- lib/core/utils/result.dart
+++ lib/core/utils/result.dart
@@ -11,6 +11,14 @@ Result<T> err<T>(Failure failure) => Left(failure);
 typedef VoidResult = Result<Unit>;
 typedef AsyncVoidResult = Future<VoidResult>;
 
+AsyncResult<T> okAsync<T>(T value) => Future.value(ok(value));
+AsyncVoidResult okVoidAsync() => Future.value(okUnit());
+
+AsyncResult<T> errAsync<T>(Failure failure) => Future.value(err<T>(failure));
+AsyncVoidResult errVoidAsync (Failure failure) async {
+  return err<Unit>(failure);
+}
+
 extension ResultX<T> on Result<T> {
   T? getOrNull() => fold((_) => null, (r) => r);
   Failure? failureOrNull() => fold((l) => l, (_) => null);
