0e4c0a1a435d9e899b05f4a506c4a4266e90876b
DeanFebrio <febriodean@gmail.com>
2025-09-15 21:50:53 +0700

    Add edit goat shed feature

--- lib/core/services/domain/init_goat_shed_domain.dart
+++ lib/core/services/domain/init_goat_shed_domain.dart
@@ -1,5 +1,9 @@
 import 'package:mbelys/core/services/service_locator.dart';
 import 'package:mbelys/features/goat_shed/domain/repositories/goat_shed_repository.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_image_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_location_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_name_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_total_goats_usecase.dart';
 import 'package:mbelys/features/goat_shed/domain/usecases/create_goat_shed_usecase.dart';
 import 'package:mbelys/features/goat_shed/domain/usecases/get_goat_shed_detail_usecase.dart';
 import 'package:mbelys/features/goat_shed/domain/usecases/get_goat_shed_list_usecase.dart';
@@ -15,5 +19,21 @@ Future<void> initGoatShedDomain () async {
 
   sl.registerFactory<GetGoatShedDetailUseCase>(() => GetGoatShedDetailUseCase(
       goatShedRepository: sl<GoatShedRepository>()
+  ));
+
+  sl.registerFactory<ChangeShedNameUseCase>(() => ChangeShedNameUseCase(
+      repository: sl<GoatShedRepository>()
+  ));
+
+  sl.registerFactory<ChangeShedLocationUseCase>(() => ChangeShedLocationUseCase(
+      repository: sl<GoatShedRepository>()
+  ));
+
+  sl.registerFactory<ChangeTotalGoatsUseCase>(() => ChangeTotalGoatsUseCase(
+      repository: sl<GoatShedRepository>()
+  ));
+
+  sl.registerFactory<ChangeShedImageUseCase>(() => ChangeShedImageUseCase(
+      repository: sl<GoatShedRepository>()
   ));
 }
--- lib/core/services/presentation/init_goat_shed_presentation.dart
+++ lib/core/services/presentation/init_goat_shed_presentation.dart
@@ -1,8 +1,13 @@
 import 'package:mbelys/core/services/service_locator.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_image_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_location_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_shed_name_usecase.dart';
+import 'package:mbelys/features/goat_shed/domain/usecases/change_total_goats_usecase.dart';
 import 'package:mbelys/features/goat_shed/domain/usecases/create_goat_shed_usecase.dart';
 import 'package:mbelys/features/goat_shed/domain/usecases/get_goat_shed_detail_usecase.dart';
 import 'package:mbelys/features/goat_shed/presentation/viewmodel/add_viewmodel.dart';
 import 'package:mbelys/features/goat_shed/presentation/viewmodel/detail_viewmodel.dart';
+import 'package:mbelys/features/goat_shed/presentation/viewmodel/edit_viewmodel.dart';
 import 'package:mbelys/features/user/presentation/viewmodels/profile_viewmodel.dart';
 
 Future<void> initGoatShedPresentation () async {
@@ -17,4 +22,14 @@ Future<void> initGoatShedPresentation () async {
               shedId: shedId
           )
   );
+
+  sl.registerFactoryParam<EditViewModel, String, void>((shedId, _) {
+    return EditViewModel(
+      changeShedNameUseCase: sl<ChangeShedNameUseCase>(),
+      changeShedLocationUseCase: sl<ChangeShedLocationUseCase>(),
+      changeShedImageUseCase: sl<ChangeShedImageUseCase>(),
+      changeTotalGoatsUseCase: sl<ChangeTotalGoatsUseCase>(),
+      detailViewModel: sl<DetailViewModel>(param1: shedId),
+    );
+  });
 }
